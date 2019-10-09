5ba586b51b948f66a5dbbb95951a1fa646785de0
firasest <jlassifiras6@gmail.com>
2019-10-09 11:18:57 +0100

    commit

--- app/src/main/java/com/mariemfelah/BasicLinearLayout/MainActivity.java
+++ app/src/main/java/com/mariemfelah/BasicLinearLayout/MainActivity.java
@@ -1,8 +1,10 @@
-package com.mariemfelah.linearlayout;
+package com.mariemfelah.BasicLinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.mariemfelah.linearlayout.R;
 
 public class MainActivity extends AppCompatActivity {
 
